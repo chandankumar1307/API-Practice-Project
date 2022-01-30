4105bc8df36f130fe5aa7f31952f7c78ae682c56
Aayush Sharma <aayusharma1401@gmail.com>
2022-01-30 23:01:52 +0530

    Tree Api Fetched

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Tree from "./components/Tree"
 
 const App = () => {
-  return <div> HELLO</div>;
+  return (
+    <div className="app">
+      <Routes>
+       <Route exact path="/tree" element={<Tree/>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
