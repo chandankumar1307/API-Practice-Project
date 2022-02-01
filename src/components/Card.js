b258ff6ee435cfb259b90648a1b680beea749385
Aayush <aayush5emails@gmail.com>
2022-02-01 22:08:25 +0530

    form-added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-const Card = () => {
+const Card = (props) => {
   const URL =
-    "https://api.data.gov.in/resource/6606a86a-5a66-4da5-b60b-75486d5e6cae?format=json&offset=0&api-key=579b464db66ec23bdd0000010cfb1359b8e4494947d998cecd368b7a&limit=37";
+    "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001816d1fb98c5c44de41bd02d36ca7365f&format=json&offset=0&limit=1000";
 
-  const [receive, setReceive] = useState("Chandan");
+  const [receive, setReceive] = useState();
 
   const fetchData = () => {
     fetch(URL)
@@ -21,11 +21,9 @@ const Card = () => {
     fetchData();
   }, []);
 
-  console.log(receive);
-
   return (
     <div>
-      <h1>Inside Card</h1>
+      <h1>{}</h1>
     </div>
   );
 };
--- src/components/Pollution.js
+++ src/components/Pollution.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Card from "./Card";
 import CardUI from "./CardUI";
+import Form from "./Form";
 
 const Pollution = () => {
-  console.log("Hello");
+  // console.log("Hello");
+  let MainCity;
+  const DataHandler = (city) => {
+    MainCity = city;
+    console.log(MainCity);
+  };
+
   return (
     <div>
       <h1>Air Quality Index</h1>
-      <Card />
+      <Form OnSaveData={DataHandler}></Form>
+
+      <Card city={MainCity} />
       <CardUI />
     </div>
   );
